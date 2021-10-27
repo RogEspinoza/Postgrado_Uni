3e9d80824d19457c88cf645531973ebfbf3bf442
rogespinoza <rogespinoza1992@gmail.com>
2021-10-26 23:22:24 -0600

    ajuste

--- Escritorio/App_Vuelos_UI/DAL/dalPasajero.cs
+++ Escritorio/App_Vuelos_UI/DAL/dalPasajero.cs
@@ -11,7 +11,16 @@ namespace DAL
     public class dalPasajero
     {
         public static Pasajero ObtenerPasajero()
-        {
+        //{
+        //    var objPasajero=new Pasajero;
+            //var DT = fn.Leer("Usp_Sel_Documentos", COD_DOC);
+            //if (DT.Rows.Count > 0)
+            //{
+            //    oDocumentosEntity.COD_DOC = (int)DT.Rows[0][0];
+            //    oDocumentosEntity.NOM_DOC = DT.Rows[0][1].ToString();
+            //    oDocumentosEntity.ESTADO = DT.Rows[0][2].ToString();
+            //}
+            //return oDocumentosEntity;
 
         } 
 
