34d82ad726d08b1a8a4517e0b5f01f31ec216b64
respinoza <respinoza@minsa.gob.ni>
2021-11-15 15:19:11 -0600

    Se realiza vinculación de hoja js

---
+++ Web/Site_Reserva_Vuelos/Site_Reserva_Vuelos/Recursos/Js/tarifa.js
@@ -0,0 +1,44 @@
+﻿var Tarifa = {
+    //events: {
+    //    'change select#listaServicios': 'Contenedores_change',
+    //    'click button#btn-buscarVeh': 'BuscarGarantia',
+    //    'click button#btn-buscarFier': 'BuscarGarantia'
+    //},
+    init: function () {
+        //self = this;
+        //Object.keys(self.events).forEach(function (e) {
+        //    $(e.split(' ')[1]).bind(e.split(' ')[0], self[self.events[e]]);
+        //});
+        console.log('se ha cargado correctamente')
+    },
+
+    //PETICIONES AJAX
+    //ObtenerListaServicios: function (token) {
+    //    return JSON.parse($.ajax({
+    //        type: "POST",
+    //        url: "../../Monox/Services/Tasacion.asmx/ObtenerServiciosGarantia",
+    //        crossDomain: true,
+    //        async: false,
+    //        data: "{'Token':'" + token + "'}",
+    //        dataType: "json",
+    //        contentType: "application/json; charset=utf-8",
+    //        beforeSend: function () {
+    //            Consulta.MostrarLoader();
+    //        },
+    //        success: function (respuesta, indice) {
+    //            return JSON.stringify(respuesta.d);
+    //        },
+    //        error: function (xhr) {
+    //            console.error('Error de Ejecución: %O:', xhr);
+    //        },
+    //        complete: function () {
+    //            Consulta.MostrarLoader();
+    //        }
+    //    }).responseText);
+    //},
+};
+
+//se carga al iniciar la pagina
+$(document).ready(function () {
+    Tarifa.init();
+});
